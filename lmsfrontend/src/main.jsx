9242dfcc8ac9851ae92c8fe04285823245800a74
Aman kumar <aman2562004@gmail.com>
2024-01-28 20:18:19 +0530

    added react-hot-toast

--- lmsfrontend/src/main.jsx
+++ lmsfrontend/src/main.jsx
@@ -1,19 +1,23 @@
 // component imports
 // tailwindcss imports
-import './index.css'
+import './index.css';
 
 // Library imports
-import ReactDOM from 'react-dom/client'
-import{BrowserRouter} from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import{Toaster} from 'react-hot-toast';
+import{BrowserRouter} from 'react-router-dom';
 
-import App from './App.jsx'
+import App from './App.jsx';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <App />
+<Toaster/>
+
 </BrowserRouter>
+
   
 
 )
